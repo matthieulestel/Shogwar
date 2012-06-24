b580a4fbe2354902f3a6b6806d1de1b23a399c41
Matthieu Lestel <matthieu.lestel@gmail.com>
2012-06-24 21:15:10 +0200

    pour débuter app rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shogwar::Application.config.secret_token = '91c605e7c268144d3c60707deec2273ad83bd50751fa505a214aa3986bb8d9bd35c75e4ef30c971763ef9e7e69bedd9aecd643840811ba50852da3e4b0f14de6'
